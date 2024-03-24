6538dc836595dcf4dcd87dcde73b16177fbce1df
SabantsevES <SabantsevES@msk.bcs.ru>
2024-03-24 16:05:37 +0300

    fix exports strateges

--- src/auth/strategies/index.ts
+++ src/auth/strategies/index.ts
@@ -1,5 +1,4 @@
 import { AccessTokenStrategy } from './access-token.strategy'
-import { GoogleStrategy } from './google.stratagy'
 import { RefreshTokenStrategy } from './refresh-token.strategy'
 
-export const STRATEGES = [AccessTokenStrategy, RefreshTokenStrategy, GoogleStrategy]
+export const STRATEGES = [AccessTokenStrategy, RefreshTokenStrategy ]
